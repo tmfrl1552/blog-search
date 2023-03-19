f1119f94ea7f662c8ea34f09fadb48c922cd1ff9
tmfrl1552 <tmfrl1552@naver.com>
2023-03-19 14:50:20 +0900

    [ADD] SearchPopularRes 생성

--- app-web/src/main/java/com/seulgi/services/search/SearchService.java
+++ app-web/src/main/java/com/seulgi/services/search/SearchService.java
@@ -2,6 +2,7 @@ package com.seulgi.services.search;
 
 import com.seulgi.dto.search.SearchBlogReq;
 import com.seulgi.dto.search.SearchBlogRes;
+import com.seulgi.dto.search.SearchPopularRes;
 import com.seulgi.provider.search.SearchProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,12 +14,13 @@ public class SearchService {
     private final SearchProvider searchProvider;
 
     public SearchBlogRes searchBlog(SearchBlogReq req) {
-        // todo - 여기서 뭐 더 추가로 확인해줘야 하는거나 예외처리해줘야 되는 부분 없을지 생각해보기d
+        // todo - 여기서 뭐 더 추가로 확인해줘야 하는거나 예외처리해줘야 되는 부분 없을지 생각
         return searchProvider.searchBlog(req);
     }
 
-    // todo - 인기 검색어 목록 조회 서비스 구현
-    public void getPopularKeywords() {
+    public SearchPopularRes getPopularKeywords() {
+        // todo - 인기 검색어 목록 구현을 어떻게 해야 할까?
 
+        return null;
     }
 }
