a635784becd722881b75f0ebabf968ce7b6faf0a
tmfrl1552 <tmfrl1552@naver.com>
2023-03-19 12:24:04 +0900

    [ADD] ResponseCode 코드 추가

---
+++ core/src/main/java/com/seulgi/enums/ResponseCode.java
@@ -0,0 +1,20 @@
+package com.seulgi.enums;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public enum ResponseCode {
+    RESPONSE_OK(0, "Success"),
+    INVALID_PARAM(20000, "Invalid Param"),
+    EXCEPTION_ERROR(99999, "Exception occurred"),
+    SEARCH_SERVICE_PROVIDER_ERROR(200, "search service network error");
+
+    int code;
+
+    String message;
+}
