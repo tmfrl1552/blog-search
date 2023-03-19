50caa488e694a69fe8ffc674caec0a4f05269464
tmfrl1552 <tmfrl1552@naver.com>
2023-03-19 15:21:05 +0900

    [ADD] repository 모듈 추가

--- app-web/src/main/java/com/seulgi/SearchApplication.java
+++ app-web/src/main/java/com/seulgi/SearchApplication.java
@@ -2,8 +2,10 @@ package com.seulgi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SearchApplication {
     public static void main(String[] args) {
         SpringApplication.run(SearchApplication.class, args);
