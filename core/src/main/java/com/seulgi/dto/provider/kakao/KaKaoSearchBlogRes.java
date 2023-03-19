84290f4b21ddce33454d8f72e8e5fc9adfdecfd9
tmfrl1552 <tmfrl1552@naver.com>
2023-03-19 12:24:57 +0900

    [ADD] KaKaoSearchBlogRes > 어노테이션 추가

--- core/src/main/java/com/seulgi/dto/provider/kakao/KaKaoSearchBlogRes.java
+++ core/src/main/java/com/seulgi/dto/provider/kakao/KaKaoSearchBlogRes.java
@@ -2,15 +2,17 @@ package com.seulgi.dto.provider.kakao;
 
 import com.seulgi.domain.provider.kakao.Document;
 import com.seulgi.domain.provider.kakao.Meta;
-import lombok.AccessLevel;
-import lombok.Data;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.util.List;
 
 @Data
+@Getter
+@Setter
+@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class SearchBlogRes {
+public class KaKaoSearchBlogRes {
 
     Meta meta;
 
