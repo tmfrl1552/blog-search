82f32c6f043f5506aabcfeffec04268a3c5f5e68
tmfrl1552 <tmfrl1552@naver.com>
2023-03-20 21:19:06 +0900

    [ADD] SearchRepository 생성

---
+++ repository/src/main/java/com/seulgi/repository/SearchRepository.java
@@ -0,0 +1,8 @@
+package com.seulgi.repository;
+
+import com.seulgi.Search;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SearchRepository extends JpaRepository<Search, Long> {
+
+}
