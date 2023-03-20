1429aee1377170a9e00492b4b035fdc268e32db9
tmfrl1552 <tmfrl1552@naver.com>
2023-03-21 02:41:15 +0900

    [MOD] 생성자 주입 방식 수정

--- app-web/src/main/java/com/seulgi/services/search/SearchService.java
+++ app-web/src/main/java/com/seulgi/services/search/SearchService.java
@@ -1,27 +1,32 @@
 package com.seulgi.services.search;
 
-import com.seulgi.SearchKeyword;
 import com.seulgi.domain.search.Keyword;
 import com.seulgi.dto.search.SearchBlogReq;
 import com.seulgi.dto.search.SearchBlogRes;
 import com.seulgi.dto.search.SearchPopularRes;
 import com.seulgi.provider.search.SearchProvider;
 import com.seulgi.repository.search.SearchRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class SearchService {
 
     private final SearchProvider searchProvider;
     private final SearchRepository searchRepository;
 
+    public SearchService(@Qualifier("KakaoSearchProvider") SearchProvider searchProvider,
+                         SearchRepository searchRepository) {
+        this.searchProvider = searchProvider;
+        this.searchRepository = searchRepository;
+    }
+
     public SearchBlogRes searchBlog(SearchBlogReq req) {
-        // todo - 여기서 뭐 더 추가로 확인해줘야 하는거나 예외처리해줘야 되는 부분 없을지 생각 필요
+        // todo - 여기서 뭐 더 추가로 확인해줘야 하는거나 예외처리
+        //  해줘야 되는 부분 없을지 생각 필요
         incrementScore(req.getQuery());
 
         return searchProvider.searchBlog(req);
