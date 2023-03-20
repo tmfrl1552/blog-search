5f90373f44048c9ec25d4d4992c7df5be9d53b74
tmfrl1552 <tmfrl1552@naver.com>
2023-03-20 23:57:32 +0900

    [ADD] SearchRepositoryImpl 구현

---
+++ repository/src/main/java/com/seulgi/repository/search/SearchRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.seulgi.repository.search;
+
+import com.seulgi.SearchKeyword;
+import com.seulgi.SearchKeywordInterface;
+import com.seulgi.domain.search.Keyword;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+@RequiredArgsConstructor
+public class SearchRepositoryImpl implements SearchRepository {
+
+    private final SearchJpaRepository searchRepository;
+
+    @Override
+    public void saveSearchKeyword(String keyword) {
+        SearchKeyword search = SearchKeyword.builder()
+                .keyword(keyword)
+                .createDatetime(new Timestamp(System.currentTimeMillis()))
+                .build();
+
+        searchRepository.save(search);
+    }
+
+    public List<Keyword> getPopularKeywords() {
+        List<SearchKeywordInterface> searchKeywords = searchRepository.findPopularSearchKeywords(
+                PageRequest.of(0,10));
+
+        return searchKeywords.stream()
+                .map(s -> Keyword.builder()
+                        .keyword(s.getKeyword())
+                        .score(s.getScore())
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
