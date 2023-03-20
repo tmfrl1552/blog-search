e51533c410d71000c1d72ac0c8c5f1b0cb655920
tmfrl1552 <tmfrl1552@naver.com>
2023-03-20 21:16:46 +0900

    [MOD] SearchService 수정

--- app-web/src/main/java/com/seulgi/services/search/SearchService.java
+++ app-web/src/main/java/com/seulgi/services/search/SearchService.java
@@ -4,6 +4,7 @@ import com.seulgi.dto.search.SearchBlogReq;
 import com.seulgi.dto.search.SearchBlogRes;
 import com.seulgi.dto.search.SearchPopularRes;
 import com.seulgi.provider.search.SearchProvider;
+import com.seulgi.repository.SearchJpa;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +13,10 @@ import org.springframework.stereotype.Service;
 public class SearchService {
 
     private final SearchProvider searchProvider;
+    private final SearchJpa searchJpa;
 
     public SearchBlogRes searchBlog(SearchBlogReq req) {
         // todo - 여기서 뭐 더 추가로 확인해줘야 하는거나 예외처리해줘야 되는 부분 없을지 생각
-
         incrementScore(req.getQuery());
 
         return searchProvider.searchBlog(req);
@@ -28,6 +29,7 @@ public class SearchService {
     }
 
     private void incrementScore(String keyword) {
-
+        // todo - keyword에 대해서 공백 제거 처리
+        searchJpa.saveSearchKeyword(keyword);
     }
 }
