25de9d69bc9250528904c22dd9a44c2f17a78f89
tmfrl1552 <tmfrl1552@naver.com>
2023-03-20 23:56:11 +0900

    [MOD] Keyword > score로 네이밍 변경

--- core/src/main/java/com/seulgi/domain/search/Keyword.java
+++ core/src/main/java/com/seulgi/domain/search/Keyword.java
@@ -1,14 +1,16 @@
 package com.seulgi.domain.search;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 @Data
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Keyword {
 
     String keyword;
 
-    int count;
+    int score;
 }
