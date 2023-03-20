206d23a5aa7b1596d2403724ec40ac6f22323f03
tmfrl1552 <tmfrl1552@naver.com>
2023-03-21 01:06:40 +0900

    [ADD] saveSearchKeyword에 @Async 추가

--- repository/src/main/java/com/seulgi/repository/search/SearchRepositoryImpl.java
+++ repository/src/main/java/com/seulgi/repository/search/SearchRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.seulgi.SearchKeywordInterface;
 import com.seulgi.domain.search.Keyword;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
@@ -18,6 +19,7 @@ public class SearchRepositoryImpl implements SearchRepository {
     private final SearchJpaRepository searchRepository;
 
     @Override
+    @Async
     public void saveSearchKeyword(String keyword) {
         SearchKeyword search = SearchKeyword.builder()
                 .keyword(keyword)
